f0b093a25d1ebb3e6f6683a98e9a5a11bd8a1c8f
mkserna <mperezserna8@gmail.com>
2024-04-18 14:37:26 -0500

    solve challenge one

---
+++ Seccion-1/reto-1.js
@@ -0,0 +1,25 @@
+let destino = {
+        pais: ["Mexico", "Brasil", "Bolivia"],
+        presupuestoPais: {
+            Mexico: 100000,
+            Brasil: 250000,
+            Bolivia: 150000
+        }
+};
+
+const eleccion = prompt(`Elige tu destino: ${destino.pais}`)
+let dias = parseInt (prompt("Ingrese la cantidad de días que durará ti viaje: "));
+
+let presupuesto = {
+    alojamiento: parseInt (prompt("Ingrese su presupuesto para el alojamiento: ")),
+    transporte: parseInt (prompt("Ingrese su presupuesto para el transporte: ")),
+    actividades: parseInt (prompt("Ingrese su presupuesto para las actividades: "))
+}
+
+let presupuestoTotal = (presupuesto.actividades + presupuesto.alojamiento + presupuesto.transporte) * dias
+
+console.log(`Tu destino es ${eleccion} \ Te vas a quedar ${dias} días \ Tu presupuesto total es: ${presupuestoTotal}`);
+
+if (presupuestoTotal < destino.presupuestoPais[eleccion]){
+    console.log("No puedes viajar")
+}else console.log(`Puedes viajar. Bienvenido a ${eleccion}`);
