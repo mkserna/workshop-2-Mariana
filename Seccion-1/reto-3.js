014c577a886170e1d6549303163a727c3c948e1e
mkserna <mperezserna8@gmail.com>
2024-04-18 21:46:53 -0500

    solve challenge 3

---
+++ Seccion-1/reto-3.js
@@ -0,0 +1,5 @@
+
+let presupuestoInicial = parseInt(prompt("Ingresa tu presupuesto inicial: "))
+let costViaje = parseInt(prompt("Ingresa el costo del viaje: "))
+let costAlojamiento = parseInt(prompt("Ingrese el costo del alojamiento: "))
+let costComida = parseInt(prompt("Ingrese el costo de la comida: "))
